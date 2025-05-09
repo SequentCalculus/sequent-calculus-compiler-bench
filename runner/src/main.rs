f59fab029c3648bb156d4a679eece39c481b0ffa
MarcoTz <marco.tzschentke@gmail.com>
2025-05-09 08:15:48 +0200

    added exec command to cli

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,16 +1,20 @@
 use clap::Parser;
 use lib::{benchmark::Benchmark, errors::Error};
+use std::str;
 
 #[derive(clap::Parser)]
 pub struct Args {
     #[arg(short, long, value_name = "NAME")]
     name: Option<String>,
-    /// Optional heap size in MB, default is 512
+    /// Optional: heap size in MB, default is 512
     #[arg(long)]
     heap_size: Option<usize>,
-    ///Optional skip benchmarks with existing results
+    ///Optional: skip benchmarks with existing results
     #[arg(long, short)]
     skip_existing: bool,
+    ///Optiional: Run benchmark instead of hypefine
+    #[arg(long, short)]
+    exec: bool,
 }
 
 fn main() -> Result<(), Error> {
@@ -27,7 +31,17 @@ fn main() -> Result<(), Error> {
             continue;
         }
         benchmark.compile_all()?;
-        benchmark.run_hyperfine_all()?;
+        if args.exec {
+            let out = benchmark.run_all(false)?;
+            for output in out {
+                println!(
+                    "{}",
+                    str::from_utf8(&output.stdout).expect("Could not read stdout")
+                );
+            }
+        } else {
+            benchmark.run_hyperfine_all()?;
+        }
     }
     Ok(())
 }
