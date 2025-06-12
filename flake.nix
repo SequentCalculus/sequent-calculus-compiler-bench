892dd6b8ca83db56f3b168f3eb68e74d0a02350b
MarcoTz <marco.tzschentke@gmail.com>
2025-06-12 19:01:56 +0200

    added flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{ 
+  description = "Run bash with all dependencies installed";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    effekt.url = "github:jiribenes/effekt-nix";
+  };
+
+  outputs = { self, nixpkgs,effekt,... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      effekt-lib = effekt.lib.${system};
+      effektVersion = "0.32.0";
+      effektBackends = with effekt-lib.effektBackends; [ llvm ];
+      effektBuild = effekt-lib.getEffekt { backends = effektBackends; };
+    in {
+      devShells.${system}.default = pkgs.mkShell{
+        buildInputs = [
+          effektBuild
+          #effekt-lib.mkDevShell { inherit effektVersion effektBackends; }
+          #grokking dependencies
+          pkgs.yasm
+          pkgs.cargo
+          #crate dependencies 
+          pkgs.gcc
+          pkgs.pkg-config
+          pkgs.fontconfig
+          #languages 
+          pkgs.ocaml
+          pkgs.koka
+          pkgs.smlnj
+          pkgs.mlton
+        ];
+
+        shellHook= ''
+          export name=""
+          rm -f Cargo.lock
+          '';
+
+      };
+    };
+}
