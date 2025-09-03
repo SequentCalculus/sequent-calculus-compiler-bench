4c9dfc27696aacf6a7e69e72ad51485895cbdd10
MarcoTz <marco.tzschentke@gmail.com>
2025-09-03 13:38:45 +0200

    added flake utils

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     #effekt.url = "github:jiribenes/effekt-nix";
     effekt.url = "github:MarcoTz/effekt-nix";
     scc = {
@@ -11,19 +12,19 @@
     };
   };
 
-  outputs = { self, nixpkgs, effekt, scc, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, flake-utils, effekt, scc, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+        pkgs = import nixpkgs { inherit system; };
 
-      effekt-lib = effekt.lib.${system};
-      effektBackends = with effekt-lib.effektBackends; [ llvm ];
-      effektBuild = effekt-lib.getEffekt { backends = effektBackends; };
+        effekt-lib = effekt.lib.${system};
+        effektBackends = with effekt-lib.effektBackends; [ llvm ];
+        effektBuild = effekt-lib.getEffekt { backends = effektBackends; };
 
-      rustPlatform = pkgs.rustPlatform;
+        rustPlatform = pkgs.rustPlatform;
 
-      dependencies = [
-        #compilers
+        dependencies = [
+#compilers
         pkgs.ocaml
         pkgs.koka
         pkgs.smlnj
@@ -32,48 +33,48 @@
         scc.packages.${system}.default
         pkgs.rustc
 
-        #required by rustc
+#required by rustc
         pkgs.gcc
-        #required by smlnj
+#required by smlnj
         pkgs.gnugrep
         pkgs.gnused
-        #required by mlton
+#required by mlton
         pkgs.coreutils
 
-        #hyperfine
+#hyperfine
         pkgs.hyperfine
         pkgs.bash
 
-      ];
-    in {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "Sequent-Calculus-Bench";
-        version = "0.1.0";
-        src = ./.;
-        nativeBuildInputs = [ pkgs.makeWrapper pkgs.pkgconf ];
-        buildInputs = dependencies ++ [ pkgs.fontconfig pkgs.expat ];
-        cargoDeps = rustPlatform.importCargoLock { lockFile = ./Cargo.lock; };
-        postFixup = ''
-          makeWrapper $out/bin/bench $out/bin/run-bench \
-          --set PATH "${pkgs.lib.makeBinPath dependencies}";
-        '';
-
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ self.packages.${system}.default ];
-
-        shellHook = ''
-          export name=""
-          echo "Call 'run-bench' to start benchmarks"
-        '';
-
-      };
-
-      apps.${system}.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/run-bench";
-      };
+        ];
+  in {
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "Sequent-Calculus-Bench";
+      version = "0.1.0";
+      src = ./.;
+      nativeBuildInputs = [ pkgs.makeWrapper pkgs.pkgconf ];
+      buildInputs = dependencies ++ [ pkgs.fontconfig pkgs.expat ];
+      cargoDeps = rustPlatform.importCargoLock { lockFile = ./Cargo.lock; };
+      postFixup = ''
+        makeWrapper $out/bin/bench $out/bin/run-bench \
+        --set PATH "${pkgs.lib.makeBinPath dependencies}";
+      '';
 
     };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ self.packages.${system}.default ];
+
+      shellHook = ''
+        export name=""
+        echo "Call 'run-bench' to start benchmarks"
+        '';
+
+    };
+
+    apps.${system}.default = {
+      type = "app";
+      program = "${self.packages.${system}.default}/bin/run-bench";
+    };
+
+  }); 
 }
